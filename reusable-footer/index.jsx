5d0cf751bbd6d4332c029375670f72fffd797f46
OmPrakash <iewd1285@gmail.com>
2026-07-21 13:25:51 +0530

    react fundamentals (lab)

---
+++ reusable-footer/index.jsx
@@ -0,0 +1,30 @@
+/**
+ * 2. Build a Reusable Footer
+ */
+export const Footer = () => {
+    return (
+        <footer>
+            <div class="footer-links">
+                <ul>
+                    <li><a href="#">My Business</a></li>
+                    <li><a href="#">Services</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Gallery</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                </ul>
+            </div>
+            <p>&copy; 2026 My Business. All Rights Reserved.</p>
+            <div class="footer-icons">
+                <a href="#">&#127758;</a>
+                <a href="#">&#128279;</a>
+                <a href="#">&#128246;</a>
+                <a href="#">&#128504;</a>
+            </div>
+        </footer>
+    )
+}
