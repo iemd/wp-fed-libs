ef8e7181f308424f31f07796bc9843d8a8240f1f
OmPrakash <iewd1285@gmail.com>
2026-07-24 19:37:55 +0530

    react state, hooks and routing (lab)

---
+++ color-picker-app/index.jsx
@@ -0,0 +1,18 @@
+/**
+ * 6. Build a Color Picker App
+ */
+const { useState } = React;
+
+export const ColorPicker = () => {
+    const [color, setColor] = useState("#ffffff");
+
+    const handleChange = (event) => {
+        setColor(event.target.value);
+    }
+    
+    return (
+        <div id="color-picker-container" style={{ backgroundColor: color }}>
+            <input type="color" id="color-input" value={color} onChange={handleChange} />
+        </div>
+    );
+};
